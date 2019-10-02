6913f4e8f7baa80f7bd244192316433896efa6ee
SebaGraz <sebastiano.grazzi@gmail.com>
2019-10-02 15:35:43 +0200

    creating dataframe from EDF file

--- scripts/import_real_data.jl
+++ scripts/import_real_data.jl
@@ -5,12 +5,32 @@
 # • in total there are several hours of data (massive dataframe)
 # • data are int16
 # • there are several channel, right now we choose one and we model that one
-
 using EDF
+using DataFrames
+using CSV
+using Plots
+
+OUT_DIR = joinpath(Base.source_dir(), "..", "output")
 INPUT = joinpath(Base.source_dir(), "..", "data")
-file_name = joinpath(INPUT, "chb01_01.edf")
+patient = "chb01_01"
+
+
+file_name = joinpath(INPUT, String(patient)*".edf")
 data =  EDF.read(file_name)
 
+seconds = 100
+channel = 1
+Time = 1/256:1/256:seconds
+x1 = Array{Float64,1}(data.signals[channel].samples[1:length(Time)])
 
-data.header
-data.signals
+plot(Time, x1, title = string(seconds)*" seconds sample, patient "*String(patient), label = "Channel "*string(channel) )
+FIG_PATH = "./assets/realdata.pdf"
+savefig(FIG_PATH)
+
+
+#Export dataframe
+FILENAME_OUT = joinpath(OUT_DIR,
+                        "real_data_"*string(seconds)*"_seconds.csv")
+
+df = DataFrame(time=Time, x1 = x1, x2 = [NaN for i in 1:length(Time)])
+CSV.write(FILENAME_OUT, df)
